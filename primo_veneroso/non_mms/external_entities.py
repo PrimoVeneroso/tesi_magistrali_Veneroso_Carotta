3ede82befd1e5e64ed9957cbcddc2a92a11643b4
PrimoVeneroso <primoveneroso2501@gmail.com>
2026-08-17 16:57:37 +0200

    feat: aggiunto lo script per trovare le entità che non hanno una pk (nè altri campi) e di conseguenza devono essere considerate entità a parte per lo sviluppo dell' ER e del database

---
+++ primo_veneroso/non_mms/external_entities.py
@@ -0,0 +1,35 @@
+import json 
+
+print("carico i dati")
+with open ("../fusione_sostituzione_link_dict.json","r",encoding="utf-8") as f:
+    data=json.load(f)
+
+lista_index= "index_term_synonyms"
+sinonimi_f="synonyms_foundation"
+
+#creo il set per non avere ripetizioni
+other_entities=set()
+
+for item in data:
+
+    if sinonimi_f in item and len(item.get("synonyms_foundation",""))>0:
+        for entity in item.get("synonyms_foundation",""):
+            other_entities.add(entity)
+    if lista_index in item:# and isinstance(item.get("index_term_synonyms",""),dict):
+        lista_temporanea_index=item.get("index_term_synonyms","")
+        for elemento in lista_temporanea_index:
+            if len(elemento.get("link","")) == 0: 
+                #print(elemento.get("link",""))
+                other_entities.add(elemento.get("title",""))
+
+print("creo il nuovo file")
+
+with open("lista_nuove_entity.md","w",encoding="utf") as g:
+
+    for entity in other_entities:
+        g.write(entity+";\n")    
+
+        
+    
+        
+        
