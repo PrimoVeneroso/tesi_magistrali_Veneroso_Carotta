77bdeb37da2b5eccf2d69df7d7098d8ff5e3b478
PrimoVeneroso <primoveneroso2501@gmail.com>
2026-08-18 10:22:00 +0200

    feat: aggiunti dei campi che non avevo considerato che aumentano di circa 200 le entità esterne che non hanno link nè altri dati oltre al loro title

--- primo_veneroso/non_mms/external_entities.py
+++ primo_veneroso/non_mms/external_entities.py
@@ -1,4 +1,4 @@
-import json 
+import json
 
 print("carico i dati")
 with open ("../fusione_sostituzione_link_dict.json","r",encoding="utf-8") as f:
@@ -6,6 +6,8 @@ with open ("../fusione_sostituzione_link_dict.json","r",encoding="utf-8") as f:
 
 lista_index= "index_term_synonyms"
 sinonimi_f="synonyms_foundation"
+inclusion="inclusion_mms"
+exclusion="exclusion_mms"
 
 #creo il set per non avere ripetizioni
 other_entities=set()
@@ -18,18 +20,27 @@ for item in data:
     if lista_index in item:# and isinstance(item.get("index_term_synonyms",""),dict):
         lista_temporanea_index=item.get("index_term_synonyms","")
         for elemento in lista_temporanea_index:
-            if len(elemento.get("link","")) == 0: 
+            if len(elemento.get("link","")) == 0:
                 #print(elemento.get("link",""))
                 other_entities.add(elemento.get("title",""))
+    if exclusion in item:
+        lista_temporanea_exclusion=item.get("exclusion_mms","")
+        for elemento in lista_temporanea_exclusion:
+            if len(elemento.get("link","")) == 0:
+                other_entities.add(elemento.get("title",""))
+    if inclusion in item:
+        lista_temporanea_inclusion=item.get("inclusion_mms","")
+        for elemento in lista_temporanea_inclusion:
+            other_entities.add(elemento)
 
 print("creo il nuovo file")
 
-with open("lista_nuove_entity.md","w",encoding="utf") as g:
+with open("lista_nuove_entity.csv","w",encoding="utf") as g:
 
     for entity in other_entities:
-        g.write(entity+";\n")    
+        g.write(entity+";\n")
 
-        
-    
-        
-        
+
+
+
+
